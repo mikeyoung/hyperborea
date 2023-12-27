12b1408c9a020a334f49a6378593a7422364d8dd
mikeyoung <mike@mikeyoung.org>
2023-12-27 12:42:49 -0500

    added json rendering option

--- website/hyperborea/views.py
+++ website/hyperborea/views.py
@@ -1,8 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Spell
+import json
 
 def spells(request):
     filter_by = request.GET.get("filter_by")
+    render_as = request.GET.get("render_as")
 
     spells = None
 
@@ -10,9 +13,17 @@ def spells(request):
         match filter_by:
             case "lists":
                 spells = Spell.objects.filter(description__icontains='<uL>')
+        match filter_by:
+            case "dash":
+                spells = Spell.objects.filter(description__icontains='-')
     else:
         spells = Spell.objects.all()
 
-    return render(request, "hyperborea/spells.html", {
-        'spells': spells
-    })
+    if render_as == "json":
+        #todo write json code that works
+        #pass
+        return JsonResponse(list(spells.values()), status=201, safe=False)
+    else:
+        return render(request, "hyperborea/spells.html", {
+            'spells': spells
+        })
