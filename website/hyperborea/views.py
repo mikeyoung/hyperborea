02dde953efcdf59cfeb7bec5f599bcc132ddb0e9
mikeyoung <mike@mikeyoung.org>
2023-12-27 11:53:49 -0500

    added filter for lists

--- website/hyperborea/views.py
+++ website/hyperborea/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 from .models import Spell
 
 def spells(request):
-    spells = Spell.objects.all()
+    filter_by = request.GET.get("filter_by")
+
+    spells = None
+
+    if filter_by != None:
+        match filter_by:
+            case "lists":
+                spells = Spell.objects.filter(description__icontains='<uL>')
+    else:
+        spells = Spell.objects.all()
 
     return render(request, "hyperborea/spells.html", {
         'spells': spells
