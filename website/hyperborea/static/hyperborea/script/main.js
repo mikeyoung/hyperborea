d9a8ecfd92aeab29a059216d5caef2a75270ce1f
mikeyoung <mike@mikeyoung.org>
2023-12-29 15:20:26 -0500

    got modal working with spell content

--- website/hyperborea/static/hyperborea/script/main.js
+++ website/hyperborea/static/hyperborea/script/main.js
@@ -13,10 +13,15 @@ const get_spell_description = (spell_id) => {
     })
     .then(response => response.json())
     .then(result => {
-        // Print result
-        console.log(result);
+        document.querySelector('#spell_modal .modal-title').innerHTML = result.spell_name;
+        document.querySelector('#spell_modal .modal-body').innerHTML = result.spell_description;
+        spell_modal.show();
     })
 }
+
+const spell_modal = new bootstrap.Modal(document.getElementById('spell_modal'), {
+    keyboard: false
+  });
 
 document.addEventListener('DOMContentLoaded', ()=>{
     document.querySelectorAll('#spell-form select').forEach((el)=>{
